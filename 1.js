7e1916aab741e864227137d544fd359a92d4a377
Kevin Radley <kevin.radley@learnet.se>
2022-09-09 15:25:14 +0200

    1 try

--- 1.js
+++ 1.js
@@ -1,5 +1,15 @@
 var ps = require("prompt-sync");
 var prompt = ps();
 
+let randomNumber = parseInt(Math. random(1) * 100);
+console.log(randomNumber)
+
 
 let gessTheNumber = prompt("Gess a number between 1-100: ");
+
+if (randomNumber == gessTheNumber) {
+    console.log('Victory!')
+}
+else if (randomNumber != gessTheNumber) {
+    console.log('try again')
+}
