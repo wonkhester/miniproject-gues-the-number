4f23c154783653dbac3447814c2d5fa0720f1ae9
Kevin Radley <kevin.radley@learnet.se>
2022-09-12 12:02:50 +0200

    almost done

--- 1.js
+++ 1.js
@@ -1,15 +1,52 @@
 var ps = require("prompt-sync");
 var prompt = ps();
 
-let randomNumber = parseInt(Math. random(1) * 100);
-console.log(randomNumber)
+var rePlay = 'yes';
+var winStreak = 0;
+while ((rePlay == 'yes') || (rePlay == 'Yes')) {
 
+    let randomNumber = parseInt(Math.random() * 100) + 1;
+    console.log(randomNumber)
 
-let gessTheNumber = prompt("Gess a number between 1-100: ");
+    var minNumber = 1;
+    var maxNumber = 100;
+    var counter = 0;
 
-if (randomNumber == gessTheNumber) {
-    console.log('Victory!')
-}
-else if (randomNumber != gessTheNumber) {
-    console.log('try again')
+    while (true){
+        if (counter == 10) {
+            console.log('You are out of lives correct number was ' + randomNumber);
+            winStreak = 0;
+            break;
+        }
+        let guessTheNumber = parseInt(prompt('Guess a number between ' + minNumber + '-' + maxNumber +": "));
+
+        if ((guessTheNumber > maxNumber) || (guessTheNumber < minNumber)){
+            console.log('Breh');
+            winStreak = 0;
+            break;
+        }
+
+        if (randomNumber < guessTheNumber) {
+            console.log('The number is smaler');
+            maxNumber = (-1) + guessTheNumber 
+        }
+        else if (randomNumber > guessTheNumber) {
+            console.log('The number is larger');
+            minNumber = 1 + guessTheNumber
+        }
+        else if (randomNumber == guessTheNumber) {
+            console.log('Victory!');
+            winStreak = winStreak + 1;
+            break;
+        }
+        else {
+            console.log('error');
+            winStreak = 0;
+            break;
+        }
+        counter = counter + 1;
+    }
+    console.log(winStreak)
+    var rePlay = prompt('play again?: ')
+    
 }
